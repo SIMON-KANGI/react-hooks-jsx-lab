a71b7764dbe868370cc244c5e0299de63ac713ee
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-01-30 11:38:56 +0300

    completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,11 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  <div id="home">
+  <h1 style={{color:"firebrick"}}>Simon is a web developer from Nairobi</h1>
+  </div>
+  )
 }
 
 export default Home;
